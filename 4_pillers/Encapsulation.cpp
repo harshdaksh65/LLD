6f8fc5367093358cd115b2bb1d42e7417fce83a6
Harsh Kumar <harshdaksh082@gmail.com>
2026-06-13 12:48:48 +0530

    improve deposit, withdraw, and transfer methods

--- 4_pillers/Encapsulation.cpp
+++ 4_pillers/Encapsulation.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 using namespace std;
 
-
-
 class Bank{
     private:
         int balance;
-    public:
         string name;
         int acc_number;
-    
+    public:
     Bank(string name,int acc_number){
         this->name = name;
         this->acc_number = acc_number;
@@ -17,13 +14,20 @@ class Bank{
     }
 
     void deposit(int amount){
+        if(amount <= 0) {
+            cout<< "invalid amount" << endl;
+            return;
+        }
         balance += amount;
-        cout<< amount << " Deposited to " << this->name << endl;
+        cout<< amount << " Deposited to self" << endl;
     }
 
     void withdraw(int amount){
-        if(balance < 0){
-            return ;
+        if(amount > balance ){
+            cout << "Insufficient balance" << endl;
+        }
+        else if(amount <=0){
+            cout << "invalid amount" << endl;
         }
         balance -= amount;
     }
@@ -37,23 +41,44 @@ class Bank{
         cout << "Account Holder Name: " << name << endl;
     }
 
+    void transferamount(Bank &b,int amount){
+        if(this->balance < amount){
+            cout<< "Low Balance" << endl;
+            return;
+        }
+        b.balance += amount;
+        this->balance -= amount;
+        cout << amount << " transfer to " << b.name << endl;
+    }
+    string BankAccountHolderName(){
+        return this->name;
+    }
+
 };
-
-
-
 
 
 
 int main(){
 
     Bank b("Harsh", 1234);
+    Bank c("Tea stall", 2255);
 
     b.details();
-    cout<< "INR: " << b.checkbalance() << endl;
+    cout<< b.BankAccountHolderName() <<" -> Balance: INR " << b.checkbalance() << endl;
 
-
+    
     b.deposit(1000);
-    cout<< "INR: " << b.checkbalance() << endl;
+    cout<< b.BankAccountHolderName() <<" -> Balance: INR " << b.checkbalance() << endl;
+
+    
+    c.details();
+    c.deposit(5000);
+
+    c.transferamount(b, 3000);
+    cout<< c.BankAccountHolderName() << " -> Balance: INR " << c.checkbalance() << endl;
+
+    cout<< b.BankAccountHolderName() <<" -> Balance: INR " << b.checkbalance() << endl;
+
 
 
     return 0;
